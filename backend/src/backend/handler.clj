224e9a508a1409ec5a4cbf87df648547641b62ec
michlampert <michlampert@gmail.com>
2022-11-26 13:07:35 +0100

    rewrite db

--- backend/src/backend/handler.clj
+++ backend/src/backend/handler.clj
@@ -3,31 +3,53 @@
             [ring.util.http-response :refer :all]
             [schema.core :as s]))
 
-(def db (ref {:user {:id "12345"
-                     :stamp-hash "00000"}
-              :admin {}}))
+(def user-db (ref { :0 {:name "michał"}
+                   :1 {:name "wojtek"}}))
+(def client-db (ref { :0 {:name "kebab"}
+                     :1 {:name "pizza"}}))
+(def stamp-db (ref { :0 {:client-id :0
+                         :user-id :0
+                         :hash "123"}
+                    :1 {:client-id :0
+                        :user-id :1
+                        :hash "321"}
+                    :2 {:client-id :1
+                        :user-id :0
+                        :hash "234"}}))
 
 (def app
   (api
-    {:swagger
-     {:ui "/"
-      :spec "/swagger.json"
-      :data {:info {:title "Lokal foods API"
-                    :description "Lokal foods API"}
-             :tags [{:name "api", :description "some apis"}]}}}
+   {:swagger
+    {:ui "/"
+     :spec "/swagger.json"
+     :data {:info {:title "Lokal foods API"
+                   :description "Lokal foods API"}
+            :tags [{:name "api", :description "some apis"}]}}}
 
-    (context "/api" []
-      :tags ["api"]
+   (context "/api" []
+     :tags ["api"]
 
-      (POST "/stamp/add" []
-        :body-params [stamp-hash :- String
-                      id :- String]
-        :summary "Checks stamp hash and sends information to client via websocket."
-      (let [user (:user @db)]
-        (when (and (= (:id user) id)
-                   (= (:stamp-hash user) stamp-hash))
-          (ok {:success true}))))
+     (POST "/stamp/add" []
+       :body-params [stamp-hash :- String
+                     user-id :- String
+                     client-id :- String]
+       :summary "Checks stamp hash and sends information to client via websocket."
+       (when (not (and (contains? @user-db (keyword user-id)) (contains? @client-db (keyword client-db)))) (ok {:success false}))
+        (let [new-id (rand-int 1000)]
+          (dosync (alter stamp-db assoc (keyword (str new-id)) {:client-id (keyword client-id) :user-id (keyword user-id)}))
+          (ok {:success true}))
+        )
 
-      (GET "/healthz" []
-        :summary "Checks if server is healthy."
-        (ok {:success true})))))
+       (POST "/stamp/status" []
+         :body-params [client-id :- String user-id :- String]
+         :summary "Check user stamps status"
+
+         (if (not (and (contains? @user-db (keyword user-id)) (contains? @client-db (keyword client-id))))
+           (ok {:success false})
+           (let [stamps (filter #(and (= (:client-id %) (keyword client-id)) (= (:user-id %) (keyword user-id))) (vals @stamp-db))]
+             (ok {:success true :result (count stamps)})))
+         )
+
+         (GET "/healthz" []
+           :summary "Checks if server is healthy."
+           (ok {:success true})))))
