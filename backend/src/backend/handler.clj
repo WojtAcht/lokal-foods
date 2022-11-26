9d53d77004fb03ee511dc8993a522fadb91478d9
WojtAcht <wachtelik@gmail.com>
2022-11-26 13:27:33 +0100

    Fix - contains? doesnt work for maps

--- backend/src/backend/handler.clj
+++ backend/src/backend/handler.clj
@@ -3,13 +3,13 @@
             [ring.util.http-response :refer :all]
             [schema.core :as s]))
 
-(def user-db (ref { :0 {:name "michał"}
+(def user-db (ref {:0 {:name "michał"}
                    :1 {:name "wojtek"}}))
-(def client-db (ref { :0 {:name "kebab"}
+(def client-db (ref {:0 {:name "kebab"}
                      :1 {:name "pizza"}}))
-(def stamp-db (ref { :0 {:client-id :0
-                         :user-id :0
-                         :hash "123"}
+(def stamp-db (ref {:0 {:client-id :0
+                        :user-id :0
+                        :hash "123"}
                     :1 {:client-id :0
                         :user-id :1
                         :hash "321"}
@@ -34,21 +34,28 @@
                      user-id :- String
                      client-id :- String]
        :summary "Checks stamp hash and sends information to client via websocket."
-       (when (not (and (contains? @user-db (keyword user-id)) (contains? @client-db (keyword client-db)))) (ok {:success false}))
-        (let [new-id (rand-int 1000)]
-          (dosync (alter stamp-db assoc (keyword (str new-id)) {:client-id (keyword client-id) :user-id (keyword user-id)}))
-          (ok {:success true}))
-        )
+       (if-not (and ((keyword user-id) @user-db)
+                    ((keyword client-id) @client-db))
+         (ok {:success false})
+         (let [new-id (rand-int 1000)
+               new-kw-id (keyword (str new-id))]
+           (dosync
+            (alter stamp-db assoc new-kw-id {:client-id (keyword client-id)
+                                             :user-id (keyword user-id)
+                                             :stamp-hash stamp-hash}))
+           (ok {:success true}))))
 
        (POST "/stamp/status" []
          :body-params [client-id :- String user-id :- String]
          :summary "Check user stamps status"
-
-         (if (not (and (contains? @user-db (keyword user-id)) (contains? @client-db (keyword client-id))))
+         (if-not (and ((keyword user-id) @user-db)
+                      ((keyword client-id) @client-db))
            (ok {:success false})
-           (let [stamps (filter #(and (= (:client-id %) (keyword client-id)) (= (:user-id %) (keyword user-id))) (vals @stamp-db))]
-             (ok {:success true :result (count stamps)})))
-         )
+           (let [stamps (->> @stamp-db
+                             vals
+                             (filter #(and (= (:client-id %) (keyword client-id))
+                                           (= (:user-id %) (keyword user-id)))))]
+             (ok {:success true :result (count stamps)}))))
 
          (GET "/healthz" []
            :summary "Checks if server is healthy."
